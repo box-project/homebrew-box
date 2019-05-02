8f2e1b69f241b3d79a6741531312bec40ac2304a
KEINOS <github@keinos.com>
2019-05-02 15:33:40 +0900

    fix: redundant version with the URL
    Fix: https://github.com/KEINOS/homebrew-box/issues/1#issuecomment-488564580

--- Formula/box.rb
+++ Formula/box.rb
@@ -2,7 +2,6 @@ class Box < Formula
   desc "📦🚀 Fast, zero config application bundler with PHARs"
   homepage "https://github.com/humbug/box"
   url "https://github.com/humbug/box/releases/download/3.0.0/box.phar"
-  version "3.0.0"
   sha256 "397f012cae0e7124cc2f949daa45cef594bfe2784c700fe33724dea43ce89777"
 
   bottle :unneeded
