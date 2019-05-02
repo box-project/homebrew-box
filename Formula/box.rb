f6e870eae8c678532622fecfe3efd93ac92e9764
KEINOS <github@keinos.com>
2019-05-02 15:06:21 +0900

    fix: Homebrew Coding Style
    Fix of issue: https://github.com/KEINOS/homebrew-box/issues/1

--- Formula/box.rb
+++ Formula/box.rb
@@ -1,19 +1,19 @@
 class Box < Formula
-  desc "📦🚀 Fast, zero config application bundler with PHARs."
+  desc "📦🚀 Fast, zero config application bundler with PHARs"
   homepage "https://github.com/humbug/box"
   url "https://github.com/humbug/box/releases/download/3.0.0/box.phar"
-  sha256 "397f012cae0e7124cc2f949daa45cef594bfe2784c700fe33724dea43ce89777"
   version "3.0.0"
+  sha256 "397f012cae0e7124cc2f949daa45cef594bfe2784c700fe33724dea43ce89777"
 
   bottle :unneeded
 
   depends_on "php" if MacOS.version <= :el_capitan
 
   def install
-      bin.install "box.phar" => "box"
+    bin.install "box.phar" => "box"
   end
 
   test do
-      shell_output("#{bin}/box --version").include?(version)
+    shell_output("#{bin}/box --version").include?(version)
   end
 end
