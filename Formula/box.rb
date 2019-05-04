11444465a94800d3e91e7b786fd2e7d76509ed86
KEINOS <github@keinos.com>
2019-05-05 04:52:01 +0900

    Upgrade to Box 3.7 (#3)

--- Formula/box.rb
+++ Formula/box.rb
@@ -1,19 +1,18 @@
 class Box < Formula
-  desc "📦🚀 Fast, zero config application bundler with PHARs."
+  desc "📦🚀 Fast, zero config application bundler with PHARs"
   homepage "https://github.com/humbug/box"
-  url "https://github.com/humbug/box/releases/download/3.0.0/box.phar"
-  sha256 "397f012cae0e7124cc2f949daa45cef594bfe2784c700fe33724dea43ce89777"
-  version "3.0.0"
+  url "https://github.com/humbug/box/releases/download/3.7.0/box.phar"
+  sha256 "57daab20d12c9eeec2cf244b0f1966327de369a66d2f59ed3bda6a3f5e9f05a9"
 
   bottle :unneeded
 
   depends_on "php" if MacOS.version <= :el_capitan
 
   def install
-      bin.install "box.phar" => "box"
+    bin.install "box.phar" => "box"
   end
 
   test do
-      shell_output("#{bin}/box --version").include?(version)
+    shell_output("#{bin}/box --version").include?(version)
   end
 end
