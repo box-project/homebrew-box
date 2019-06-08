b215d5841796a6217d9de5bb118880ffc2d4fc26
Théo FIDRY <theo.fidry@gmail.com>
2019-06-08 16:28:35 +0200

    Update to 3.8.0

--- Formula/box.rb
+++ Formula/box.rb
@@ -1,8 +1,8 @@
 class Box < Formula
   desc "📦🚀 Fast, zero config application bundler with PHARs"
   homepage "https://github.com/humbug/box"
-  url "https://github.com/humbug/box/releases/download/3.7.0/box.phar"
-  sha256 "57daab20d12c9eeec2cf244b0f1966327de369a66d2f59ed3bda6a3f5e9f05a9"
+  url "https://github.com/humbug/box/releases/download/3.8.0/box.phar"
+  sha256 "48cf39f95667e20173a3835799951e1f237187cec8ca55d950c3cf1c6035efc4"
 
   bottle :unneeded
 
