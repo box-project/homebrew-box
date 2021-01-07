d2598b68b6f1fa33509d79a3fb0b86795032d1d8
Alexander Schranz <alexander@sulu.io>
2021-01-07 15:09:54 +0100

    Update box version (#5)

--- Formula/box.rb
+++ Formula/box.rb
@@ -1,8 +1,8 @@
 class Box < Formula
   desc "📦🚀 Fast, zero config application bundler with PHARs"
   homepage "https://github.com/humbug/box"
-  url "https://github.com/humbug/box/releases/download/3.8.4/box.phar"
-  sha256 "f2058b50b126f1e9e811538ef2688cdbf578ae24f881de6d32a5f30c4eb6e3fb"
+  url "https://github.com/humbug/box/releases/download/3.11.1/box.phar"
+  sha256 "49145e1a644807816481de29611722f881da151e1402806acbfe03a18e00fa0e"
 
   bottle :unneeded
 
