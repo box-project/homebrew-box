11dd4fc8190130aa5f56eff420fa0813f9da0c43
Owen Voke <development@voke.dev>
2021-06-29 13:00:41 +0100

    chore: update Box to v3.13.0

--- Formula/box.rb
+++ Formula/box.rb
@@ -1,8 +1,8 @@
 class Box < Formula
   desc "📦🚀 Fast, zero config application bundler with PHARs"
-  homepage "https://github.com/humbug/box"
-  url "https://github.com/humbug/box/releases/download/3.11.1/box.phar"
-  sha256 "49145e1a644807816481de29611722f881da151e1402806acbfe03a18e00fa0e"
+  homepage "https://github.com/box-project/box"
+  url "https://github.com/box-project/box/releases/download/3.13.0/box.phar"
+  sha256 "b438b65fee5d633ee7d322086bebcea0910079e53c1f2dccb6f449f9fd3fa2e2"
 
   bottle :unneeded
 
