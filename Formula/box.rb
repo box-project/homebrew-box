5704dbf8a8ee2a4d9a2fa871ef08ceea2e1a8b04
Abdouni Abdelkarim <abdounikarim@gmail.com>
2022-06-01 12:09:58 +0200

    Update box.rb
    Hello, 
    
    I just updated version from 3.14.0 to 3.16.0 and associated sha with this command :  `shasum -a 256 box.phar`

--- Formula/box.rb
+++ Formula/box.rb
@@ -1,8 +1,8 @@
 class Box < Formula
   desc "📦🚀 Fast, zero config application bundler with PHARs"
   homepage "https://github.com/box-project/box"
-  url "https://github.com/box-project/box/releases/download/3.14.0/box.phar"
-  sha256 "dbbd211f7690845a5e545105b569be7b7b2c671c2b2e0dfab571c708562e7bf4"
+  url "https://github.com/box-project/box/releases/download/3.16.0/box.phar"
+  sha256 "f508e28f309d7e95a319bdcd5f13dcfbb18eb91cb7a6cac9b69bc7799d78bdf9"
 
   depends_on "php" if MacOS.version <= :el_capitan
 
