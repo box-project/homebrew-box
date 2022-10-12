e79c1cc3967350a277094d2be6c2a4ffce75f537
hussainweb <hussainweb@gmail.com>
2022-10-12 17:54:34 -0400

    Update box formula to 4.1.0 (#11)

--- Formula/box.rb
+++ Formula/box.rb
@@ -1,8 +1,8 @@
 class Box < Formula
   desc "📦🚀 Fast, zero config application bundler with PHARs"
   homepage "https://github.com/box-project/box"
-  url "https://github.com/box-project/box/releases/download/3.16.0/box.phar"
-  sha256 "f508e28f309d7e95a319bdcd5f13dcfbb18eb91cb7a6cac9b69bc7799d78bdf9"
+  url "https://github.com/box-project/box/releases/download/4.1.0/box.phar"
+  sha256 "0edad3b02a1c0a92e782210faca06b15e53e3a7343f00a08f3783e63bfb31f3b"
 
   depends_on "php" if MacOS.version <= :el_capitan
 
